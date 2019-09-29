e36f7fa211a748809712c8ebde1d4be6fa9161fe
1571221458@qq.com <majiafei>
2019-09-29 18:58:23 +0800

    首次提交

---
+++ manggo-admin/src/main/java/com/mjf/mango/manggoadmin/ManggoAdminApplication.java
@@ -0,0 +1,13 @@
+package com.mjf.mango.manggoadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManggoAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManggoAdminApplication.class, args);
+    }
+
+}
