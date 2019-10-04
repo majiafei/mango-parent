163fddf6603caace3e3ed4c018232908e2e5752d
fury36 <40300926+fury36@users.noreply.github.com>
2019-10-04 16:18:07 +0800

    jwt

---
+++ manggo-admin/src/main/java/com/mjf/mango/manggoadmin/common/entity/JwtConstants.java
@@ -0,0 +1,12 @@
+package com.mjf.mango.manggoadmin.common.entity;
+
+/**
+ * @ProjectName: yitao-parent
+ * @Auther: majiafei
+ * @Date: 2019/5/6 19:00
+ * @Description:
+ */
+public class JwtConstants {
+    public static final String JWT_KEY_ID = "id";
+    public static final String JWT_KEY_USER_NAME = "username";
+}
