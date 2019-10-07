ceea67cbbc448c2e83b816f5254b7a7475f0f706
fury36 <40300926+fury36@users.noreply.github.com>
2019-10-07 18:10:51 +0800

    机构树

--- doc/sql/sql.sql
+++ doc/sql/sql.sql
@@ -131,25 +131,20 @@ CREATE TABLE `sys_user` (
   UNIQUE KEY `UNIQUE_USER_NAME` (`user_name`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE `sys_user` (
-  `user_id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '用户id',
-  `user_name` varchar(50) NOT NULL COMMENT '用户名',
-  `user_nick_name` varchar(150) DEFAULT NULL COMMENT '昵称',
-  `user_avatar` varchar(255) DEFAULT NULL COMMENT '头像',
-  `user_password` varchar(100) NOT NULL COMMENT '密码',
-  `user_salt` varchar(40) NOT NULL COMMENT '加密盐',
-  `user_email` varchar(100) DEFAULT NULL COMMENT '邮箱',
-  `user_mobile` varchar(20) DEFAULT NULL COMMENT '手机号',
-  `user_staus` tinyint(4) DEFAULT NULL COMMENT '状态   0:禁用，1：正常',
-  `dept_id` bigint(20) DEFAULT NULL COMMENT '机构id',
-  `create_by` varchar(50) DEFAULT NULL COMMENT '创建人',
-  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
-  `last_update_by` varchar(50) DEFAULT NULL COMMENT '更新人',
-  `last_update_time` datetime DEFAULT NULL COMMENT '更新时间',
-  `del_flag` tinyint(4) DEFAULT NULL COMMENT '是否删除  -1:已删除   0:正常',
-  PRIMARY KEY (`user_id`),
-  UNIQUE KEY `UNIQUE_USER_NAME` (`user_name`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+DROP TABLE IF EXISTS `sys_user_role`;
+CREATE TABLE `sys_user_role`
+(
+  `id`               bigint(20) NOT NULL AUTO_INCREMENT COMMENT '编号',
+  `user_id`          bigint(20)  DEFAULT NULL COMMENT '用户ID',
+  `role_id`          bigint(20)  DEFAULT NULL COMMENT '角色ID',
+  `create_by`        varchar(50) DEFAULT NULL COMMENT '创建人',
+  `create_time`      datetime    DEFAULT NULL COMMENT '创建时间',
+  `last_update_by`   varchar(50) DEFAULT NULL COMMENT '更新人',
+  `last_update_time` datetime    DEFAULT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 88
+  DEFAULT CHARSET = utf8 COMMENT ='用户角色';
 
 DROP TABLE IF EXISTS `sys_role_menu`;
 CREATE TABLE `sys_role_menu` (
