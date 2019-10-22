d6f0f967ff6ca9c6ca5e6beda808e5422aac4b6d
fury36 <40300926+fury36@users.noreply.github.com>
2019-10-22 16:20:47 +0800

    打印pdf

--- manggo-admin/src/main/java/com/mjf/mango/manggoadmin/controller/UserController.java
+++ manggo-admin/src/main/java/com/mjf/mango/manggoadmin/controller/UserController.java
@@ -1,8 +1,12 @@
 package com.mjf.mango.manggoadmin.controller;
 
 import com.google.code.kaptcha.impl.DefaultKaptcha;
+import com.itextpdf.text.*;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.pdf.*;
 import com.mango.common.ResponseResult;
 import com.mango.common.entity.PageRequest;
+import com.mango.common.utils.PdfUtils;
 import com.mjf.mango.manggoadmin.aspect.LoginLog;
 import com.mjf.mango.manggoadmin.dto.UserDTO;
 import com.mjf.mango.manggoadmin.common.exception.ServiceException;
@@ -24,9 +28,11 @@ import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.Date;
 import java.util.Set;
 
 /**
@@ -149,4 +155,53 @@ public class UserController {
         return ResponseResult.ok(permissions);
     }
 
+    @GetMapping("/printPdf")
+    public void printPdf(HttpServletResponse response) {
+        try {
+            Document document = new Document();
+            PdfWriter.getInstance(document, response.getOutputStream());
+            response.setContentType("application/pdf");
+            document.open();
+            document.newPage();
+            // A4纸张
+            document.setPageSize(PageSize.A4);
+            // 标题
+            document.addTitle("用户信息");
+            // 表格，3列
+            PdfPTable table = new PdfPTable(3);
+            // 单元格
+            PdfPCell cell = null;
+            // 字体，定义为蓝色加粗
+            Font f8 = new Font();
+            f8.setStyle(Font.BOLD);
+            f8.setColor(BaseColor.BLUE);
+            // 标题
+            cell = new PdfPCell(new Paragraph("id", f8));
+            // 居中对齐
+            cell.setHorizontalAlignment(1);
+            // 将单元格加入表格
+            table.addCell(cell);
+
+            // 标题
+            cell = new PdfPCell(new Paragraph("age", f8));
+            // 居中对齐
+            cell.setHorizontalAlignment(1);
+            // 将单元格加入表格
+            table.addCell(cell);
+
+            // 标题
+            cell = new PdfPCell(new Paragraph("name", f8));
+            // 居中对齐
+            cell.setHorizontalAlignment(1);
+            // 将单元格加入表格
+            table.addCell(cell);
+            // 文档中加入表格
+            document.add(table);
+            document.close();
+        } catch (DocumentException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
